4fb8febf650839c5d184b8479d56ecf0727a2941
Jona <jonathan.pc.tech@gmail.com>
2016-08-09 16:39:29 -0400

    added better handling and parsing of html
    better handles errors and more information returned + added colors :o

--- xerox_warranty_tool.py
+++ xerox_warranty_tool.py
@@ -3,28 +3,54 @@
 import sys
 import requests
 import math
+from bs4 import BeautifulSoup
 
 serials = "xerox.serials"
 count = 0
 
-def check_warranty(serial):
-    payload = {'serial': serial}
-    r = requests.post('https://www.office.xerox.com/smart/entitlements/index.cfm?fuseaction=entitlements.findEntitlement', params=payload)
-    if "Active" not in r.content:
-        print '[*]', serial, 'is: \033[1;31mExpired\033[1;m'
-    else:
-        print '[*]', serial, 'is: \033[1;32mActive\033[1;m'
+def get_warranty_status(serial):
 
-print "********************************"
-print " Xerox Warranty Checker v0.1"
-print "********************************"
-print "[*] Opening Serial list:",
+    payload = {'serial': serial}
+    try:
+        r = requests.post('https://www.office.xerox.com/smart/entitlements/index.cfm?fuseaction=entitlements.findEntitlement', params=payload)
+    except Exception:
+        print "[!] Failed while contacting the XEROX website"
+
+    s = BeautifulSoup(r.text, 'html.parser')
+    tb = s.find_all('table')[0]
+    rw = tb.find_all('tr')[2:]
+
+    print "\033[0;37;40m[*] Serial:\033[1;32;40m", serial
+
+    for x in range(4):
+        for row in rw:
+            cols = row.find_all('td')
+            if cols[0].get_text() == "":
+                return
+            if "The product is not covered" in cols[0].get_text():
+                print "\033[1;37;40m[!]\033[1;31;1m WARRANTY EXPIRED!"
+                return
+            print "\033[0;37;40m[*]\t" + cols[0].get_text(),
+            if cols[1].get_text() == "Active":
+                print "\033[3;32;40m" + cols[1].get_text()
+            elif cols[1].get_text() == "Expired":
+                print "\033[3;31;1m" + cols[1].get_text()
+            else:
+                print "\033[3;37;40m" + cols[1].get_text()
+
+
+print "\033[1;37;40m********************************"
+print "\033[25;31;40m Xerox Warranty Checker v0.1"
+print "\033[1;37;40m********************************"
+print "[-] Opening Serial list:",
 try:
     with open(serials, "r") as f:
         print "Success"
+        print "[-] Contacting Xerox"
         for serial in f:
             count +=1
-            check_warranty(serial.strip())
-    print "[*] Checked Xerox Warranty for", count , "Supplied Serials"
+            get_warranty_status(serial.strip())
+    print "\033[0;34;40m[*] Checked Xerox warranty for", count , "supplied serial number(s)"
 except Exception:
-    print "[!] Failed while accesing or sending serials"
+    print "[!] Something Bad..."
+sys.exit(0)
